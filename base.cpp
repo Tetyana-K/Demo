617815996b9b282ba2def43118727728757c7555
unknown <tanya.kyryk@gmail.com>
2020-10-21 23:18:57 +0300

    first commit

---
+++ base.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+#include <filesystem>
+using namespace std::tr2::sys;
+
+int main3()
+{
+	ofstream f("a.txt");
+	f << "This text we are writing to text file(a.txt)" << endl;
+	f.close();
+	copy_file(path("a.txt"), path("b.txt")); // копіювання файлу
+	rename(path("b.txt"),  path("newb.txt")); // перейменування файлу
+	remove(path("a.txt")); // видалення файлу
+
+	return 0;
+}
