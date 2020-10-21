81e3e01159ab9665d9f19956d3d27ca51e4124c2
unknown <tanya.kyryk@gmail.com>
2020-10-21 23:39:07 +0300

    new comment

--- base.cpp
+++ base.cpp
@@ -6,7 +6,7 @@ using namespace std::tr2::sys;
 
 int main3()
 {
-	ofstream f("a.txt");
+	ofstream f("a.txt"); // відкрили потік
 	f << "This text we are writing to text file(a.txt)" << endl;
 	f.close();
 	copy_file(path("a.txt"), path("b.txt")); // копіювання файлу
